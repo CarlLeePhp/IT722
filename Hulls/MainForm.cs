5f0ff0a4fdd43f3e6a6f72cf267857d6d554f08e
Carl Li <leekunhui@sina.cn>
2021-07-30 22:55:36 +1200

    merge

--- Hulls/MainForm.cs
+++ Hulls/MainForm.cs
@@ -65,6 +65,8 @@ namespace Hulls
                     }
                     
                 }
+                // draw circle
+
             }
             
 
@@ -123,8 +125,73 @@ namespace Hulls
                     hullPoints.RemoveAt(hullPoints.Count - 2);
                 }
             }
+
+            // get circle
+            // c^2 = a^2 + b^2 - 2ab cosC
+            // cosC = (a^2 + b^2 - c^2) / 2ab
+            double cosC = -1;
+            Point pointA = hullPoints[0];
+            Point pointB = hullPoints[1];
+            Point pointC;
+            for(int i=2; i < hullPoints.Count; i++)
+            {
+                double c = Distance(pointA, pointB);
+                double a = Distance(pointB, hullPoints[i]);
+                double b = Distance(pointA, hullPoints[i]);
+                double cos = (Math.Pow(a, 2) + Math.Pow(b, 2) - Math.Pow(c, 2)) / (2 * a * b);
+                if (cos > cosC)
+                {
+                    cosC = cos;
+                    pointC = hullPoints[i];
+                }
+            }
+
             isRadio = true;
             pictureBox.Refresh();
+        }
+
+        private Circle GetCircleFromTreePoints(Point pointA, Point pointB, Point pointC)
+        {
+            double maxEdge;
+            double a = Distance(pointB, pointC);
+            double b = Distance(pointC, pointA);
+            double c = Distance(pointA, pointB);
+            double cosA = GetCosByPoints(pointB, pointC, pointA);
+            double cosB = GetCosByPoints(pointC, pointA, pointB);
+            double cosC = GetCosByPoints(pointA, pointB, pointC);
+            double cosMin = cosA;
+            maxEdge = a;
+            Point center = new Point((pointB.X + pointC.X) / 2, (pointB.Y + pointC.Y) / 2); ;
+            double radius;
+            if (cosB < cosMin)
+            {
+                cosMin = cosB;
+                maxEdge = b;
+                center = new Point((pointA.X + pointC.X)/2, (pointA.Y + pointC.Y)/2);
+            }
+            if (cosC < cosMin)
+            {
+                cosMin = cosC;
+                maxEdge = c;
+                center = new Point((pointA.X + pointB.X) / 2, (pointA.Y + pointB.Y) / 2);
+            }
+            if(cosMin <= 0) // > 90 degree
+            {
+                radius = maxEdge / 2;
+                return new Circle(center.X, center.Y, radius);
+            }
+
+            return new Circle(0, 0, 0);
+                
+            
+        }
+        private double GetCosByPoints(Point pointA, Point pointB, Point pointC)
+        {
+            double c = Distance(pointA, pointB);
+            double a = Distance(pointB, pointC);
+            double b = Distance(pointA, pointC);
+            double cos = (Math.Pow(a, 2) + Math.Pow(b, 2) - Math.Pow(c, 2)) / (2 * a * b);
+            return cos;
         }
         private bool CheckHull()
         {
@@ -135,6 +202,13 @@ namespace Hulls
         private double SignedArea(Point a, Point b, Point c)
         {
             return a.X * b.Y + a.Y * c.X + b.X * c.Y - a.X * c.Y - b.X * a.Y - c.X * b.Y;
+        }
+        private double Distance(Point a, Point b)
+        {
+            return Math.Sqrt(
+                Math.Pow(a.X - b.X, 2)
+                + Math.Pow(a.Y - b.Y, 2)
+                );
         }
         private void SwapPoints(int indexOne, int indexTwo)
         {
